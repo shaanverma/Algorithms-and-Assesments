0566a4f6e1f458a1624f09a5b5d9353738ea56d8
Shaan Verma <shaan.k.verma@gmail.com>
2024-12-27 20:41:40 -0500

    Adding question instructions

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,7 @@
+'''
+Q: Create a function that takes two strings and returns True if either of the strings is a palindrome, else return False.
+A palindrome is a word that is the same forwards and backwards. For example, "racecar" and "bob" are palindromes.
+'''
 def palindrome(word1, word2):
     return word1.upper() == word2.upper()[::-1]
 
